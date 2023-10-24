fcc06d169a5e26dc28734ef82a4a19aaf87f3b94
rorense <rkorense@gmail.com>
2023-10-24 18:18:01 +1300

    Set up of authentication page

---
+++ netflix/component/Input.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div className='relative'>
+        <input id='email'
+        className='
+            block
+            rounded-md
+            pt-6
+            pb-1
+            w-full
+            text-md
+            text-white
+            bg-neutral-700
+            appearance-none
+            focus:outline-none
+            focus:ring-0
+            peer
+        ' 
+        placeholder=' ' />
+        <label htmlFor="email" className='
+            absolue
+            text-md
+        '>
+
+        </label>
+    </div>
+  )
+}
+
+export default Input
