5bd2012bcb5f69f8ba0cae3329512dd3209134d1
rorense <rkorense@gmail.com>
2023-10-31 15:55:19 +1300

    implemented login feature

--- netflix/app/api/register/route.ts
+++ netflix/app/api/register/route.ts
@@ -1,27 +1,28 @@
-import bcrypt from 'bcrypt';
-import { NextResponse } from 'next/server';
-import prismadb from '@/libs/prismadb';
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import prismadb from "@/libs/prismadb";
 
 export async function POST(req: Request) {
-    // Only want POST request
-    if (req.method !== 'POST') {
-      return new NextResponse("prohibited method", { status: 405 });
-    }
+  // Only want POST request
+  if (req.method !== "POST") {
+    return new NextResponse("prohibited method", { status: 405 });
+  }
 
-    try {
-      const body = await req.json();
-      const { email, name, password } = body;
+  // Finding unique email in the
+  try {
+    const body = await req.json();
+    const { email, name, password } = body;
 
     const existingUser = await prismadb.user.findUnique({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
 
     if (existingUser) {
       return new NextResponse("Email already taken", { status: 422 });
     }
-    
+
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const user = await prismadb.user.create({
@@ -29,14 +30,14 @@ export async function POST(req: Request) {
         email,
         name,
         hashedPassword,
-        image: '',
+        image: "",
         emailVerified: new Date(),
-      }
-    })
+      },
+    });
 
-    console.log("User created successfully")
-    return NextResponse.json({ user }, {status: 200})   
+    console.log("User created successfully");
+    return NextResponse.json({ user }, { status: 200 });
   } catch (error) {
-    return new NextResponse("Something went wrong", {status: 400 })
+    return new NextResponse("Something went wrong", { status: 400 });
   }
 }
--- netflix/app/auth/page.tsx
+++ netflix/app/auth/page.tsx
@@ -1,79 +1,112 @@
-'use client'
-import Input from '@/components/Input'
-import React, { useCallback, useState } from 'react'
-import axios from 'axios';
+"use client";
+import Input from "@/components/Input";
+import React, { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Auth() {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   // A state for toggling login and signup state
-  const [variant, setVariant] = useState('login');
+  const [variant, setVariant] = useState("login");
 
   // Toggling between login and signup state
   const toggleVariant = useCallback(() => {
-    setVariant((currentvariant) => currentvariant == 'login' ? 'register': 'login')
-  }, [])
+    setVariant((currentvariant) =>
+      currentvariant == "login" ? "register" : "login"
+    );
+  }, []);
+
+  // Callback function for login
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
 
   // callback function for registering user
-  const register = useCallback(async() => {
+  const register = useCallback(async () => {
     try {
-      await axios.post('/api/register', {
+      await axios.post("/api/register", {
         email,
         name,
         password,
       });
+
+      login();
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }, [email, name, password]);
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className='px-12 py-5'>
-          <img src="./images/logo.png" alt="logo" className='h-12'/>
+        <nav className="px-12 py-5">
+          <img src="./images/logo.png" alt="logo" className="h-12" />
         </nav>
-        <div className='flex justify-center'>
-          <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
-            <h2 className='text-white text-4xl mb-8 font-semibold'>
-              {variant == 'login' ? 'Sign In' : 'Register'}
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant == "login" ? "Sign In" : "Register"}
             </h2>
-            <div className='flex flex-col gap-4'>
-              {variant == 'register' && (
-              <Input 
-                label='Username' 
-                onChange={(e: any) => setName(e.target.value)} 
-                id='name'
-                value={name}  />
+            <div className="flex flex-col gap-4">
+              {variant == "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  value={name}
+                />
               )}
 
-              <Input 
-                label='Email' 
-                onChange={(e: any) => setEmail(e.target.value)} 
-                id='email'
+              <Input
+                label="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
                 value={email}
-                type='email'  />
-              <Input 
-                label='Password' 
-                onChange={(e: any) => setPassword(e.target.value)} 
-                id='password'
+                type="email"
+              />
+              <Input
+                label="Password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                id="password"
                 value={password}
-                type='password'  />
+                type="password"
+              />
             </div>
-            <button onClick={register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition' >
-              {variant == 'login' ? 'Login' : 'Sign Up'}
+            <button
+              onClick={variant == "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant == "login" ? "Login" : "Sign Up"}
             </button>
-            <p className='text-neutral-500 mt-12 text-sm'>
-              {variant == 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span className='text-white text-sm ml-3 hover:underline cursor-pointer' onClick={toggleVariant}>
-                {variant == 'register' ? 'Log In' : 'Create an account'}
+            <p className="text-neutral-500 mt-12 text-sm">
+              {variant == "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                className="text-white text-sm ml-3 hover:underline cursor-pointer"
+                onClick={toggleVariant}
+              >
+                {variant == "register" ? "Log In" : "Create an account"}
               </span>
             </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
