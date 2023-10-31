f1087b7752ae9d274d2fe3dcbbd2283e2420cc0b
rorense <rkorense@gmail.com>
2023-10-31 16:05:00 +1300

    adding icons

--- netflix/app/auth/page.tsx
+++ netflix/app/auth/page.tsx
@@ -4,6 +4,8 @@ import React, { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export default function Auth() {
   const router = useRouter();
@@ -93,6 +95,15 @@ export default function Auth() {
             >
               {variant == "login" ? "Login" : "Sign Up"}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={30} />
+              </div>
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12 text-sm">
               {variant == "login"
                 ? "First time using Netflix?"
